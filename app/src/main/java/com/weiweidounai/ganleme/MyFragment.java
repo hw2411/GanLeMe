a5392fcae50818ecebc7d4235266d60bcc695a3a
hw2411 <hw2411@163.com>
2016-03-03 09:42:01 +0800

    construct Fragment
    from FragmentDemo

---
+++ app/src/main/java/com/weiweidounai/ganleme/MyFragment.java
@@ -0,0 +1,30 @@
+package com.weiweidounai.ganleme;
+
+/**
+ * Created by haha on 2016/3/2.
+ */
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+public class MyFragment extends Fragment {
+
+    private String content;
+    public MyFragment(){
+    };
+    @SuppressLint("ValidFragment")
+    public MyFragment(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg_content,container,false);
+        TextView txt_content = (TextView) view.findViewById(R.id.txt_content);
+        txt_content.setText(content);
+        return view;
+    }
+}
